88071c38311d29573c2a1e26634325128c5b984f
flame <zhengtianji001@163.com>
2017-06-15 09:08:55 +0800

    ## 修改目录结构

--- run.py
+++ run.py
@@ -1,17 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
-@app.route('/')
-def index():
-    return 'Index Page'
-
-@app.route('/hello')
+@app.route('/note')
 def hello():
-    return 'Hello World'
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
